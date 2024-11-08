78e4e7df1e22850196eb3b849e51e615f86d593c
nferdica <nferdica@outlook.com>
2024-11-07 22:38:59 -0300

    feat: Creating new controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello world!!";
 });
+
+Route::get('/about', function () {
+    echo "ABout us";
+});
+
+Route::get('/main', [MainController::class, 'index']);
