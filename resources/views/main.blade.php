54ae5ae1f61c51526344f34d9d653ddee8eeda46
nferdica <nferdica@outlook.com>
2024-11-14 21:47:49 -0300

    feat: Creating new view and blade template

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome, view and blade</h1>
+    <hr>
+    <h3>The value is: {{ $value }}</h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/about', function () {
     echo "ABout us";
 });
 
-Route::get('/main', [MainController::class, 'index']);
+Route::get('/main/{value}', [MainController::class, 'index']);
